27e053ebaf7620948b360c0e6a3f00d6838f0e56
yangyc <yangyc96@yeah.net>
2019-08-23 18:12:49 +0800

    添加@Configuration注解

--- src/main/java/com/yyc/personalbolg/PersonalbolgApplication.java
+++ src/main/java/com/yyc/personalbolg/PersonalbolgApplication.java
@@ -2,7 +2,9 @@ package com.yyc.personalbolg;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 @SpringBootApplication
 public class PersonalbolgApplication {
 
