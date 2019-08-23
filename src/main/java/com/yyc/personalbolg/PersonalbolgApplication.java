2dad5a0aa2dc17e39550bc2f4858fc644f3a0bc4
yangyc <yangyc96@yeah.net>
2019-08-23 15:53:41 +0800

    搭建springboot框架

---
+++ src/main/java/com/yyc/personalbolg/PersonalbolgApplication.java
@@ -0,0 +1,13 @@
+package com.yyc.personalbolg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalbolgApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PersonalbolgApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yyc/personalbolg/PersonalbolgApplicationTests.java
@@ -0,0 +1,16 @@
+package com.yyc.personalbolg;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PersonalbolgApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
