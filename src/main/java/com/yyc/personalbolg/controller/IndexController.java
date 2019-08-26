b53d0d960683ba27f86460e79c8ad414bdaeb50d
yangyc <yangyc96@yeah.net>
2019-08-26 18:09:09 +0800

    新增页面和方法

--- src/main/java/com/yyc/personalbolg/controller/IndexController.java
+++ src/main/java/com/yyc/personalbolg/controller/IndexController.java
@@ -4,15 +4,28 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/")
 public class IndexController {
 
-    @RequestMapping("/index")
+    @RequestMapping("index")
     public String showMe(){
         return "index";
     }
 
-    @RequestMapping("/hello")
-    public String hello(){
-        return "";
+    @RequestMapping("javaBase")
+    public String javaBase(){
+        return "javaBase";
+    }
+    @RequestMapping("selfIntroduce")
+    public String selfIntroduce(){
+        return "selfIntroduce";
+    }
+    @RequestMapping("readBase")
+    public String readBase(){
+        return "readBase";
+    }
+    @RequestMapping("readEnvironment")
+    public String readEnvironment(){
+        return "readEnvironment";
     }
 }
